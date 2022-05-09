841efb5dac03de519c287f6d1e39918ab6b62b68
Jessee Chow <admin@cnbbx.com>
2022-05-09 15:41:42 +0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -9,3 +9,5 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+sed -i '$a src-git alist https://github.com/zhoujinshi/openwrt-alist' feeds.conf.default
