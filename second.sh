3fb6410c17b33b0602d86b1df80b87b580dc90b2
Cnbbx <admin@cnbbx.com>
2022-12-16 19:15:20 +0800

    Update second.sh

--- second.sh
+++ second.sh
@@ -10,15 +10,11 @@
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/LEDE/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
-sed -i '/set wireless.radio${devidx}.disabled/d' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/Cnbbx/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/Cnbbx/g' package/base-files/files/bin/config_generate
 
-# Modify the version number
-sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION='R22.03" >> package/base-files/files/etc/openwrt_release
-sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='cnbbx build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt '" >> package/base-files/files/etc/openwrt_release
+# Cancel power on and disable WIFI
+sed -i '/set wireless.radio${devidx}.disabled/d' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
@@ -27,12 +23,26 @@ sed -i 's/bootstrap/argonne/g' feeds/luci/modules/luci-base/root/etc/config/luci
 # Modify aria2
 sed -i 's/+ariang//g'  feeds/luci/applications/luci-app-aria2/Makefile
 
+# Rom Size
+echo '# Rom Size' >> .config
+echo 'CONFIG_TARGET_KERNEL_PARTSIZE=64' >> .config
+echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=800' >> .config
+
+# Modify the version number
+echo '# Image Configurations' >> .config
+echo 'CONFIG_IMAGEOPT=y' >> .config
+echo 'CONFIG_VERSIONOPT=y' >> .config
+echo "CONFIG_VERSION_NUMBER='Cnbbx build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt R22.03'" >> .config
+echo 'CONFIG_VERSION_DIST="Cnbbx"' >> .config
+echo 'CONFIG_VERSION_FILENAMES=y' >> .config
+echo 'CONFIG_VERSION_HOME_URL="http://youku.i.cnbbx.com/"' >> .config
+
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="cnbbx"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"cnbbx"@' .config
+    echo 'CONFIG_KERNEL_BUILD_USER="Cnbbx"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"Cnbbx"@' .config
 
 # Add kernel build domain
 [ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
     echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config 
